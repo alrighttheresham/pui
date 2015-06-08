d17e50d1300d1655ae6dc9a13bd09baac3d0ac8f
Damian ONeill <damian.oneill@gmail.com>
2015-06-08 10:58:31 +0100

    added in global flags for server address, username and password

--- pui.go
+++ pui.go
@@ -14,6 +14,23 @@ func main() {
 	app.Author = "Damian ONeill"
 	app.Email = "doneill@btisystems.com"
 	app.Commands = Commands
-
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "server, s",
+			Value:  "localhost",
+			Usage:  "IP Address of the PSM Server",
+			EnvVar: "PSM_SERVER",
+		}, cli.StringFlag{
+			Name:   "username, u",
+			Value:  "admin",
+			Usage:  "Username for the PSM Server",
+			EnvVar: "PSM_USERNAME",
+		}, cli.StringFlag{
+			Name:   "password, p",
+			Value:  "admin",
+			Usage:  "Password for the PSM Server",
+			EnvVar: "PSM_PASSWORD",
+		},
+	}
 	app.Run(os.Args)
 }
